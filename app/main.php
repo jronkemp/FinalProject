1beea081433c91d8139f60ecc69c9a6457f6633f
tayloryoung <youngtal@iu.edu>
2021-12-14 13:08:20 -0500

    Fixed Bearer Authentication, now works with users that have a hashed password (users that were created after the original creation of the database will work). Also fixed error with update routes

---
+++ app/main.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Author: Taylor Young
+ * Date: 12/13/2021
+ * File: main.php
+ * Description: the main section
+ */
+?>
+<!-- Begin section content -->
+<main role="main" class="flex-shrink-0">
+    <section class="section">
+        <div class="main-heading">
+            <div class="container" id="main-heading"><!-- Main heading: content is generated at runtime --></div>
+        </div>
+        <div class="sub-heading">
+            <div class="container" id="sub-heading"><!-- Sub heading: content is generated at runtime--></div>
+        </div>
+        <div class="section-content">
+            <div class="container" id="section-content"><!-- Section content: content is generated at runtime--></div>
+        </div>
+    </section>
+</main>
+<!-- End section content -->
